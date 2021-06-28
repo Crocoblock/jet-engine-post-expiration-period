51c9236e6f513db0fc3971e4c77b163232034272
Andrew Shevchenko <andremjhead@gmail.com>
2021-06-28 11:57:21 +0300

    Update jet-engine-post-expiration-period.php

--- jet-engine-post-expiration-period.php
+++ jet-engine-post-expiration-period.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Plugin Name: JetEngine - post expiration period
+ * Plugin Name: JetEngine Post Expiration Period Module
  * Plugin URI:
  * Description:
- * Version:     1.0.0
+ * Version:     1.0.1
  * Author:      Crocoblock
  * Author URI:  https://crocoblock.com/
  * Text Domain: jet-engine-post-expiration-period
@@ -22,7 +22,7 @@ add_action( 'plugins_loaded', 'jet_engine_post_ep_init' );
 
 function jet_engine_post_ep_init() {
 
-    define( 'JET_ENGINE_POST_EP_VERSION', '1.0.0' );
+    define( 'JET_ENGINE_POST_EP_VERSION', '1.0.1' );
 
     define( 'JET_ENGINE_POST_EP__FILE__', __FILE__ );
     define( 'JET_ENGINE_POST_EP_PLUGIN_BASE', plugin_basename( JET_ENGINE_POST_EP__FILE__ ) );
@@ -31,7 +31,3 @@ function jet_engine_post_ep_init() {
 
     require JET_ENGINE_POST_EP_PATH . 'plugin.php';
 }
-
-
-
-
